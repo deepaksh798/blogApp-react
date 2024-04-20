c5db5fd3245628a5ea8df2f7234347fe488fccdd
deepaksh798 <deepak.sh798@gmail.com>
2024-04-20 19:20:07 +0530

    rte

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,6 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
-import Input from "./Input";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, RTE };
